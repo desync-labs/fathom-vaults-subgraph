aaaedfe187df1570c7db5b1aefb3d24cb8690f4a
Anton Grigorev <anton.grigorev.dev@gmail.com>
2023-12-29 19:27:26 +0400

    Update config

--- config/addresses.template.ts
+++ config/addresses.template.ts
@@ -1,10 +1,6 @@
 // AS compiler does not like interface
 export class Addresses {
     FathomVault: string
-    StrategyManager: string
-    SharesManager: string
-    Governance: string
-    Setters: string
     blockNumber: string
     network: string
   }
@@ -12,10 +8,6 @@ export class Addresses {
   // AS compiler does not like const
   export let addresses: Addresses = {
     FathomVault: '{{FathomVault}}',
-    StrategyManager: '{{StrategyManager}}',
-    SharesManager: '{{SharesManager}}',
-    Governance: '{{Governance}}',
-    Setters: '{{Setters}}',
     blockNumber: '{{blockNumber}}',
     network: '{{network}}'
   }
