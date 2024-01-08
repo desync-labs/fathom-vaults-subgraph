52b9d6319e03e3fbf6798d9cb421001e74049c6a
ViniciusTenenteSecurrency <vinicius.tenente@securrency.com>
2024-01-08 15:20:37 -0300

    Fix zero value on APR

--- src/utils/strategy/strategy-report-result.ts
+++ src/utils/strategy/strategy-report-result.ts
@@ -12,62 +12,70 @@ export function create(
   previousReport: StrategyReport,
   currentReport: StrategyReport
 ): StrategyReportResult {
-  let txHash = transaction.hash.toHexString();
-  log.debug(
-    '[StrategyReportResult] Create strategy report result between previous {} and current report {}. Strategy {} TxHash: {}',
-    [previousReport.id, currentReport.id, currentReport.strategy, txHash]
-  );
-
-  let id = buildIdFromTransaction(transaction);
-  let strategyReportResult = new StrategyReportResult(id);
-  strategyReportResult.timestamp = transaction.timestamp;
-  strategyReportResult.blockNumber = transaction.blockNumber;
-  strategyReportResult.currentReport = currentReport.id;
-  strategyReportResult.previousReport = previousReport.id;
-  strategyReportResult.startTimestamp = previousReport.timestamp;
-  strategyReportResult.endTimestamp = currentReport.timestamp;
-  strategyReportResult.duration = currentReport.timestamp
-    .toBigDecimal()
-    .minus(previousReport.timestamp.toBigDecimal());
-  strategyReportResult.durationPr = BIGDECIMAL_ZERO;
-  strategyReportResult.apr = BIGDECIMAL_ZERO;
-  strategyReportResult.transaction = transaction.id;
-
-  let profit = currentReport.gain.minus(previousReport.gain);
-  let msInDays = strategyReportResult.duration.div(MS_PER_DAY);
-  log.info(
-    '[StrategyReportResult] Report Result - Start / End: {} / {} - Duration: {} (days {}) - Profit: {} - TxHash: {}',
-    [
-      strategyReportResult.startTimestamp.toString(),
-      strategyReportResult.endTimestamp.toString(),
-      strategyReportResult.duration.toString(),
-      msInDays.toString(),
-      profit.toString(),
-      txHash,
-    ]
-  );
-
-  if (!previousReport.currentDebt.isZero() && !msInDays.equals(BIGDECIMAL_ZERO)) {
-    let profitOverTotalDebt = profit
-      .toBigDecimal()
-      .div(previousReport.currentDebt.toBigDecimal());
-    strategyReportResult.durationPr = profitOverTotalDebt;
-    let yearOverDuration = DAYS_PER_YEAR.div(msInDays);
-    let apr = profitOverTotalDebt.times(yearOverDuration);
-
+  if (currentReport.id == previousReport.id) {
     log.info(
-      '[StrategyReportResult] Report Result - Duration: {} ms / {} days - Duration (Year): {} - Profit / Total Debt: {} / APR: {} - TxHash: {}',
+      '[StrategyReportResult] Previous report {} and current report {} are the same. No strategy report result created.',
+      [previousReport.id, currentReport.id]
+    );
+    return null;
+  } else {
+    let txHash = transaction.hash.toHexString();
+    log.info(
+      '[StrategyReportResult] Create strategy report result between previous {} and current report {}. Strategy {} TxHash: {}',
+      [previousReport.id, currentReport.id, currentReport.strategy, txHash]
+    );
+
+    let id = buildIdFromTransaction(transaction);
+    let strategyReportResult = new StrategyReportResult(id);
+    strategyReportResult.timestamp = transaction.timestamp;
+    strategyReportResult.blockNumber = transaction.blockNumber;
+    strategyReportResult.currentReport = currentReport.id;
+    strategyReportResult.previousReport = previousReport.id;
+    strategyReportResult.startTimestamp = previousReport.timestamp;
+    strategyReportResult.endTimestamp = currentReport.timestamp;
+    strategyReportResult.duration = currentReport.timestamp
+      .toBigDecimal()
+      .minus(previousReport.timestamp.toBigDecimal());
+    strategyReportResult.durationPr = BIGDECIMAL_ZERO;
+    strategyReportResult.apr = BIGDECIMAL_ZERO;
+    strategyReportResult.transaction = transaction.id;
+
+    let profit = currentReport.gain.minus(previousReport.gain);
+    let msInDays = strategyReportResult.duration.div(MS_PER_DAY);
+    log.info(
+      '[StrategyReportResult] Report Result - Start / End: {} / {} - Duration: {} (days {}) - Profit: {} - TxHash: {}',
       [
+        strategyReportResult.startTimestamp.toString(),
+        strategyReportResult.endTimestamp.toString(),
         strategyReportResult.duration.toString(),
         msInDays.toString(),
-        yearOverDuration.toString(),
-        profitOverTotalDebt.toString(),
-        apr.toString(),
+        profit.toString(),
         txHash,
       ]
     );
-    strategyReportResult.apr = apr;
+
+    if (!previousReport.currentDebt.isZero() && !msInDays.equals(BIGDECIMAL_ZERO)) {
+      let profitOverTotalDebt = profit
+        .toBigDecimal()
+        .div(previousReport.currentDebt.toBigDecimal());
+      strategyReportResult.durationPr = profitOverTotalDebt;
+      let yearOverDuration = DAYS_PER_YEAR.div(msInDays);
+      let apr = profitOverTotalDebt.times(yearOverDuration);
+
+      log.info(
+        '[StrategyReportResult] Report Result - Duration: {} ms / {} days - Duration (Year): {} - Profit / Total Debt: {} / APR: {} - TxHash: {}',
+        [
+          strategyReportResult.duration.toString(),
+          msInDays.toString(),
+          yearOverDuration.toString(),
+          profitOverTotalDebt.toString(),
+          apr.toString(),
+          txHash,
+        ]
+      );
+      strategyReportResult.apr = apr;
+    }
+    strategyReportResult.save();
+    return strategyReportResult;
   }
-  strategyReportResult.save();
-  return strategyReportResult;
 }
