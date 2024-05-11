e859fbaf09d997a1e56e23fdf6ba64427698e72a
Tsisar <fhantom.tp@gmail.com>
2024-05-11 11:02:34 +0200

    feat: added support to deploy subgraph in sepolia network, address need to be populated pending

---
+++ config/sepoliaAddressScript.ts
@@ -0,0 +1,28 @@
+import * as fs from 'fs'
+import * as mustache from 'mustache'
+import * as networkAddresses from '../networks/addresses.json'
+import { Addresses } from './addresses.template'
+
+// mustache doesn't like numbered object keys
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+let renameAddresses: any = networkAddresses
+renameAddresses['sepolia'] = networkAddresses['11155111'][process.argv[2]]["addresses"]
+
+export let addresses: Addresses = {
+  Factory: '{{sepolia.Factory}}',
+  Accountant: '{{sepolia.Accountant}}',
+  blockNumber: networkAddresses['11155111'][process.argv[2]]["blockNumber"],
+  network:  networkAddresses['11155111']["network"],
+}
+
+const main = (): void => {
+  try {
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+  } catch (e) {
+    console.log(`Error saving artifacts: ${e.message}`)
+  }
+}
+
+main()
