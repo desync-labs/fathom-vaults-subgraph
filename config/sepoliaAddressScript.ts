e2b76e6663ffe66698298018144b34b6db6a6da7
Tsisar <fhantom.tp@gmail.com>
2024-05-27 13:00:07 +0200

    Using constants for NETWORK_ID and NETWORK_NAME

--- config/sepoliaAddressScript.ts
+++ config/sepoliaAddressScript.ts
@@ -1,28 +1,31 @@
-import * as fs from 'fs'
-import * as mustache from 'mustache'
-import * as networkAddresses from '../networks/addresses.json'
-import { Addresses } from './addresses.template'
+import * as fs from 'fs';
+import * as mustache from 'mustache';
+import * as networkAddresses from '../networks/addresses.json';
+import { Addresses } from './addresses.template';
+
+// Change the NETWORK_ID and NETWORK_NAME to the appropriate values
+const NETWORK_ID = '11155111';
+const NETWORK_NAME = 'sepolia';
 
 // mustache doesn't like numbered object keys
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-
-let renameAddresses: any = networkAddresses
-renameAddresses['sepolia'] = networkAddresses['11155111'][process.argv[2]]["addresses"]
+let renameAddresses: any = networkAddresses;
+renameAddresses[NETWORK_NAME] = networkAddresses[NETWORK_ID][process.argv[2]]["addresses"];
 
 export let addresses: Addresses = {
-  Factory: '{{sepolia.Factory}}',
-  Accountant: '{{sepolia.Accountant}}',
-  blockNumber: networkAddresses['11155111'][process.argv[2]]["blockNumber"],
-  network:  networkAddresses['11155111']["network"],
-}
+  Factory: `{{${NETWORK_NAME}.Factory}}`,
+  Accountant: `{{${NETWORK_NAME}.Accountant}}`,
+  blockNumber: networkAddresses[NETWORK_ID][process.argv[2]]["blockNumber"],
+  network: networkAddresses[NETWORK_ID]["network"],
+};
 
 const main = (): void => {
   try {
-    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
-    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses));
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2));
   } catch (e) {
-    console.log(`Error saving artifacts: ${e.message}`)
+    console.log(`Error saving artifacts: ${e.message}`);
   }
-}
+};
 
-main()
+main();
--- config/xinfinAddressScript.ts
+++ config/xinfinAddressScript.ts
@@ -1,28 +1,31 @@
-import * as fs from 'fs'
-import * as mustache from 'mustache'
-import * as networkAddresses from '../networks/addresses.json'
-import { Addresses } from './addresses.template'
+import * as fs from 'fs';
+import * as mustache from 'mustache';
+import * as networkAddresses from '../networks/addresses.json';
+import { Addresses } from './addresses.template';
+
+// Change the NETWORK_ID and NETWORK_NAME to the appropriate values
+const NETWORK_ID = '50';
+const NETWORK_NAME = 'xinfin';
 
 // mustache doesn't like numbered object keys
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-
-let renameAddresses: any = networkAddresses
-renameAddresses['xinfin'] = networkAddresses['50'][process.argv[2]]["addresses"]
+let renameAddresses: any = networkAddresses;
+renameAddresses[NETWORK_NAME] = networkAddresses[NETWORK_ID][process.argv[2]]["addresses"];
 
 export let addresses: Addresses = {
-  Factory: '{{xinfin.Factory}}',
-  Accountant: '{{xinfin.Accountant}}',
-  blockNumber: networkAddresses['50'][process.argv[2]]["blockNumber"],
-  network:  networkAddresses['50']["network"],
-}
+  Factory: `{{${NETWORK_NAME}.Factory}}`,
+  Accountant: `{{${NETWORK_NAME}.Accountant}}`,
+  blockNumber: networkAddresses[NETWORK_ID][process.argv[2]]["blockNumber"],
+  network: networkAddresses[NETWORK_ID]["network"],
+};
 
 const main = (): void => {
   try {
-    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
-    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses));
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2));
   } catch (e) {
-    console.log(`Error saving artifacts: ${e.message}`)
+    console.log(`Error saving artifacts: ${e.message}`);
   }
-}
+};
 
-main()
+main();
